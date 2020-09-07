2c42f40552afa17c2da1412bfb0fcc4ee3667b29
Celia Babst <celiababst@gmail.com>
2020-09-07 17:03:52 -0500

    search()
    updated the search() fucntion.

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -51,9 +51,13 @@ bool LinkedList<T>::search(T value) const
 	Node<T>* temp = m_front;
 	bool isFound = false;
 
-	/** TODO
-		Fix this method
-	*/
+	while(temp != nullptr)
+	{
+		if(temp->getValue()==value)
+		{
+			isFound = true;
+		}
+	}
 
 	return(isFound);
 }
